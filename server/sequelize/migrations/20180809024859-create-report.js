eec22f25184c4a1f76e3250e8b2a382b4ac16c19
Guilherme GM <321.guilherme@gmail.com>
2018-08-15 00:13:32 -0300

    feat(migrations): add create report

---
+++ server/sequelize/migrations/20180809024859-create-report.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Reports', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      success: {
+        type: Sequelize.BOOLEAN
+      },
+      log: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Reports');
+  }
+};
