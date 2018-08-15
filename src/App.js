4a2bc8fc07394ffce728c0fafda03e10106b257c
Guilherme GM <321.guilherme@gmail.com>
2018-08-15 09:55:23 -0300

    feat(app): add router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import AppBar from '@material-ui/core/AppBar'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Grid from '@material-ui/core/Grid'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
@@ -8,23 +9,23 @@ import ReportTableContainer from './containers/ReportTableContainer'
 class App extends Component {
   render() {
     return (
-      <div>
+      <Router>
         <Grid container spacing={16} direction="column">
           <Grid item>
             <AppBar position="static">
               <Toolbar>
                 <Typography variant="title" color="inherit">
-                  Open Cron Monitor
+                  <Link to="/">Open Cron Monitor</Link>
                 </Typography>
               </Toolbar>
             </AppBar>
           </Grid>
 
           <Grid item>
-            <ReportTableContainer />
+            <Route exact path="/" component={ReportTableContainer} />
           </Grid>
         </Grid>
-      </div>
+      </Router>
     )
   }
 }
